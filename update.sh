99560c8203fc8e0d69748ca5421297c74c133259
tenderness72 <ck9is5@gmail.com>
2026-06-04 00:21:37 +0900

    Update update.sh to include paper_list_view

--- update.sh
+++ update.sh
@@ -2,4 +2,5 @@
 cp /home/n/Acedia_Linux/main.py /opt/acedia/main.py
 cp /home/n/Acedia_Linux/acedia/views/main_window.py /opt/acedia/acedia/views/main_window.py
 cp /home/n/Acedia_Linux/acedia/views/paper_detail_view.py /opt/acedia/acedia/views/paper_detail_view.py
+cp /home/n/Acedia_Linux/acedia/views/paper_list_view.py /opt/acedia/acedia/views/paper_list_view.py
 echo "完了"
